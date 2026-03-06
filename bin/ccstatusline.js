0bc910f51fd401ffb815f03b5577ea85f12763da
Medhat Dawoud <MedhatDawoud@gmail.com>
2026-03-06 12:42:22 +0100

    refactor: replace Node.js wrapper with self-contained shell bin
    - Merge setup-statusline.sh and statusline.sh into bin/cconeline
    - Embed statusline.sh as heredoc; no REPO_DIR resolution needed
    - Remove Node.js dependency and engines constraint from package.json
    - Bump version to 1.0.2

--- bin/ccstatusline.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-const { execFileSync } = require('child_process');
-const path = require('path');
-const script = path.join(__dirname, '..', 'setup-statusline.sh');
-execFileSync('bash', [script, ...process.argv.slice(2)], { stdio: 'inherit' });
